4ba89c588db431ef6b11d1ea4da3e96b55a4922a
Arijit Das <arijitdas.das148@gmail.com>
2026-06-28 18:24:21 +0530

    improvements in locators

--- scripts/jiohotstar-search-play-third.js
+++ scripts/jiohotstar-search-play-third.js
@@ -7,34 +7,37 @@ const jioHotstarAppPackage = env.JIOHOTSTAR_APP_PACKAGE ?? "in.startv.hotstar";
 const jioHotstarAppActivity =
   env.JIOHOTSTAR_APP_ACTIVITY ?? "com.hotstar.MainActivity";
 
-const selectors = {
-  searchButton:
-    env.JIOHOTSTAR_SEARCH_ENTRY_SELECTOR ??
-    '//android.view.View[@content-desc="Search"]',
-  searchInput:
-    env.JIOHOTSTAR_SEARCH_FIELD_SELECTOR ??
-    '//android.widget.EditText[@resource-id="tag_search_bar"]',
-  resultsPage:
-    env.JIOHOTSTAR_RESULTS_PAGE_SELECTOR ??
-    '//android.view.View[@resource-id="tag_search_results_page"]',
-  searchResults:
-    env.JIOHOTSTAR_RESULT_SELECTOR ??
-    '(//android.view.View[@resource-id="tag_search_results_page"]//android.view.View[@resource-id="tag_search_result_horizontal_card_playable"])[3]',
-};
 
-await runAndroidTest({
-  appPackage: jioHotstarAppPackage,
-  appActivity: jioHotstarAppActivity,
-  testFn: async (driver) => {
-    await click(driver, selectors.searchButton);
-    await setValue(driver, selectors.searchInput, searchText);
-    await driver.pressKeyCode(66);
-    await click(driver, selectors.searchResults);
+
+    const selectors = {
+      searchButton:
+          env.JIOHOTSTAR_SEARCH_ENTRY_SELECTOR ??
+          '//android.view.View[@content-desc="Search"]',
+      searchInput:
+          env.JIOHOTSTAR_SEARCH_FIELD_SELECTOR ??
+          '//android.widget.EditText[@resource-id="tag_search_bar"]',
+      resultsPage:
+          env.JIOHOTSTAR_RESULTS_PAGE_SELECTOR ??
+          '//android.view.View[@resource-id="tag_search_results_page"]',
+      searchResults:
+          env.JIOHOTSTAR_RESULT_SELECTOR ??
+          'android=new UiSelector().resourceId("tag_search_result_horizontal_card_playable").instance(2)',
+    };
+
+    await runAndroidTest({
+      appPackage: jioHotstarAppPackage,
+      appActivity: jioHotstarAppActivity,
+      testFn: async (driver) => {
+        await click(driver, selectors.searchButton);
+        await setValue(driver, selectors.searchInput, searchText);
+        await driver.pressKeyCode(66);
+        await click(driver, selectors.searchResults);
         console.log(
-      `Started JioHotstar playback for result of "${searchText}".`,
-    );
-    await driver.pause(30000);
+            `Started JioHotstar playback for result of "${searchText}".`,
+        );
+        await driver.pause(30000);
 
-  },
-});
+      },
+    });
+
 
--- scripts/youtube-search-play-third.js
+++ scripts/youtube-search-play-third.js
@@ -8,19 +8,22 @@ const youtubeAppActivity =
   env.YOUTUBE_APP_ACTIVITY ??
   "com.google.android.apps.youtube.app.WatchWhileActivity";
 
+// Anchor to the ScrollView, then find the specific child inside it
+//const scrollableSelector = env.YOUTUBE_RESULT_SCROLL ??'android=new UiScrollable(new UiSelector().resourceId("com.google.android.youtube:id/browse_fragment_layout_coordinator_layout")).getChildByInstance(new UiSelector().className("android.view.ViewGroup").descriptionMatches(".+"), 2)';
+const scrollableContainer = 'android=new UiScrollable(new UiSelector().resourceId("com.google.android.youtube:id/results"))';
+const targetSelector = 'new UiSelector().className("android.view.ViewGroup").descriptionMatches(".+")';
 const selectors = {
   searchButton:
-    env.YOUTUBE_SEARCH_ENTRY_SELECTOR ??
-    '//android.widget.ImageView[@content-desc="Search"]',
+      env.YOUTUBE_SEARCH_ENTRY_SELECTOR ??
+      '//android.widget.ImageView[@content-desc="Search"]',
   searchInput:
-    env.YOUTUBE_SEARCH_FIELD_SELECTOR ??
-    '//android.widget.EditText[@resource-id="com.google.android.youtube:id/search_edit_text"]',
+      env.YOUTUBE_SEARCH_FIELD_SELECTOR ??
+      '//android.widget.EditText[@resource-id="com.google.android.youtube:id/search_edit_text"]',
   resultsPage: "id=com.google.android.youtube:id/results",
   searchResults:
-    env.YOUTUBE_RESULT_SELECTOR ??
-    'android=new UiSelector().resourceId("com.google.android.youtube:id/results").childSelector(new UiSelector().className("android.view.ViewGroup").descriptionContains("").instance(2))',
-};
-
+      env.YOUTUBE_RESULT_SELECTOR ??
+      'android=new UiSelector().resourceId("com.google.android.youtube:id/results").childSelector(new UiSelector().className("android.view.ViewGroup").instance(2))',
+}
 await runAndroidTest({
   appPackage: youtubeAppPackage,
   appActivity: youtubeAppActivity,
@@ -28,7 +31,16 @@ await runAndroidTest({
     await click(driver, selectors.searchButton);
     await setValue(driver, selectors.searchInput, searchText);
     await driver.pressKeyCode(66);
-    await click(driver, selectors.searchResults);
+    const element = await driver.$(selectors.searchResults);
+    await element.waitForDisplayed({ timeout: 10000 });
+    //const scrollEle = await driver.$(scrollableSelector);
+    //await scrollEle.waitForDisplayed({ timeout: 10000 });
+
+
+    // 3. Execution: Scroll until the 3rd (index 2) item is found
+    const element1 = await driver.$(`${scrollableContainer}.scrollIntoView(${targetSelector}.instance(2))`);
+    await element1.click();
+   // await click(driver, selectors.searchResults);
         console.log(
       `Started YouTube playback for result of "${searchText}".`,
     );
